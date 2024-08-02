dc87e85179f06f9afcb1fd43ff064ed97504b12f
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-08-02 03:22:20 -0400

    i hate cocos2d.

--- src/CCKeyboardDispatcher.cpp
+++ src/CCKeyboardDispatcher.cpp
@@ -6,7 +6,11 @@ using namespace geode::prelude;
 class $modify(MyCCKeyboardDispatcher, CCKeyboardDispatcher) {
 	bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool p1 , bool p2) {
 		bool result = CCKeyboardDispatcher::dispatchKeyboardMSG(key, p1, p2);
-		if (PlayLayer::get()) { Manager::getSharedInstance()->lastKeyName = CCKeyboardDispatcher::keyToString(key); }
+		if (PlayLayer::get()) {
+			auto name = CCKeyboardDispatcher::keyToString(key);
+			if (name == nullptr) { name = "Unknown"; }
+			Manager::getSharedInstance()->lastKeyName = name;
+		}
 		else { Manager::getSharedInstance()->lastKeyName = "N/A"; }
 		return result;
 	}
